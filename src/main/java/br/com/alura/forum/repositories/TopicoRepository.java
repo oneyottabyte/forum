e2c2f389a34f72611f054234bd6c5f18a22605f8
oneyottabyte <junhinhodorian@gmail.com>
2022-01-17 18:58:51 -0300

    Testando Consulta

--- src/main/java/br/com/alura/forum/repositories/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repositories/TopicoRepository.java
@@ -1,9 +1,12 @@
 package br.com.alura.forum.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.alura.forum.models.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long>{
 
+	List<Topico> findByCursoNome(String nomeCurso);
 }
