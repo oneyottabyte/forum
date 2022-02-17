95d243a2b406edeeb6c9118dd983019c8e4d6157
Dorian <dorianc.vieira@gmail.com>
2022-02-17 13:34:50 -0300

    testes concluidos

--- src/main/java/br/com/alura/forum/controllers/AuthenticationController.java
+++ src/main/java/br/com/alura/forum/controllers/AuthenticationController.java
@@ -20,7 +20,7 @@ import br.com.alura.forum.controllers.form.LoginForm;
 
 @RestController
 @RequestMapping("/auth")
-@Profile("prod")
+@Profile(value = {"prod", "test"})
 public class AuthenticationController {
 
 	@Autowired
